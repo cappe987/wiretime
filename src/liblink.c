d72601ba5484a59007b557dda14d47c547b6a2c5
Casper Andersson <casper.casan@gmail.com>
2023-08-14 21:53:39 +0200

    liblink: Close socket and fix indentation
    Signed-off-by: Casper Andersson <casper.casan@gmail.com>

--- src/liblink.c
+++ src/liblink.c
@@ -48,37 +48,38 @@ int get_smac(int sockfd, char ifname[IFNAMSIZ], unsigned char smac[6])
 
 int get_iface_mac(char ifname[IFNAMSIZ], unsigned char mac_address[ETH_ALEN])
 {
-   struct ifreq ifr;
-    struct ifconf ifc;
-    char buf[1024];
-    int success = 0;
+	struct ifreq ifr;
+	struct ifconf ifc;
+	char buf[1024];
+	int success = 0;
 
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-    if (sock == -1) { /* handle error*/ };
+	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+	if (sock == -1) { /* handle error*/ };
 
-    ifc.ifc_len = sizeof(buf);
-    ifc.ifc_buf = buf;
-    if (ioctl(sock, SIOCGIFCONF, &ifc) == -1) { /* handle error */ }
+	ifc.ifc_len = sizeof(buf);
+	ifc.ifc_buf = buf;
+	if (ioctl(sock, SIOCGIFCONF, &ifc) == -1) { /* handle error */ }
 
-    struct ifreq* it = ifc.ifc_req;
-    const struct ifreq* const end = it + (ifc.ifc_len / sizeof(struct ifreq));
+	struct ifreq* it = ifc.ifc_req;
+	const struct ifreq* const end = it + (ifc.ifc_len / sizeof(struct ifreq));
 
-    for (; it != end; ++it) {
-        strcpy(ifr.ifr_name, it->ifr_name);
-        if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0) {
-            if (! (ifr.ifr_flags & IFF_LOOPBACK)) { // don't count loopback
-                if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0) {
-                    success = 1;
-                    break;
-                }
-            }
-        }
-        else { /* handle error */ }
-    }
+	for (; it != end; ++it) {
+		strcpy(ifr.ifr_name, it->ifr_name);
+		if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0) {
+			if (! (ifr.ifr_flags & IFF_LOOPBACK)) { // don't count loopback
+				if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0) {
+					success = 1;
+					break;
+				}
+			}
+		}
+		else { /* handle error */ }
+	}
 
 
-    if (success)
-	    memcpy(mac_address, ifr.ifr_hwaddr.sa_data, 6);
+	if (success)
+		memcpy(mac_address, ifr.ifr_hwaddr.sa_data, 6);
+	close(sock);
 }
 
 void set_dmac(unsigned char *frame, unsigned char mac[ETH_ALEN])
